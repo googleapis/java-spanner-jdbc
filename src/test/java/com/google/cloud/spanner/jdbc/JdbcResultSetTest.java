5326c6659f410b6a386a1fb0af2a66dee3568fe4
Knut Olav Løite <koloite@gmail.com>
2021-10-19 18:09:06 +0200

    chore: remove unnecessary boxing

--- src/test/java/com/google/cloud/spanner/jdbc/JdbcResultSetTest.java
+++ src/test/java/com/google/cloud/spanner/jdbc/JdbcResultSetTest.java
@@ -1251,7 +1251,7 @@ public class JdbcResultSetTest {
   @Test
   public void testGetFloatIndexFromString() throws SQLException {
     assertEquals(
-        Float.valueOf(STRING_NUMBER_VALUE),
+        Float.parseFloat(STRING_NUMBER_VALUE),
         subject.getFloat(STRING_COLINDEX_NUMBER),
         0f);
     assertFalse(subject.wasNull());
