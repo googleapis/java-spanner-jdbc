295899abacf1254746ea2999b23e159bba99ec78
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2022-05-26 21:51:15 +0530

    fix: add missing closing curly brace

--- src/test/java/com/google/cloud/spanner/jdbc/JdbcDriverTest.java
+++ src/test/java/com/google/cloud/spanner/jdbc/JdbcDriverTest.java
@@ -135,6 +135,7 @@ public class JdbcDriverTest {
       assertThat(e.getMessage())
           .contains(
               "Specify only one of credentialsUrl, encodedCredentials, credentialsProvider and OAuth token");
+    }
   }
 
   @Test
