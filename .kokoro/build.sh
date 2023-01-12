56aa79d4e5e3cb7038e178788e7f138d4c5db16c
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2023-01-13 00:43:18 +0530

    chore: add continuous tests against cloud-devel (#1094)
    * chore: add continuous tests against cloud-devel

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -69,6 +69,20 @@ integration)
       verify
     RETURN_CODE=$?
     ;;
+integration-cloud-devel)
+    mvn -B ${INTEGRATION_TEST_ARGS} \
+      -ntp \
+      -Penable-integration-tests \
+      -DtrimStackTrace=false \
+      -Dclirr.skip=true \
+      -Denforcer.skip=true \
+      -Dspanner.gce.config.server_url=https://staging-wrenchworks.sandbox.googleapis.com \
+      -Dspanner.testenv.instance=projects/span-cloud-testing/instances/spanner-testing-east1 \
+      -Dspanner.gce.config.project_id=span-cloud-testing \
+      -fae \
+      verify
+    RETURN_CODE=$?
+    ;;
 graalvm)
     # Run Unit and Integration Tests with Native Image.
     mvn -B ${INTEGRATION_TEST_ARGS} -ntp -Pnative -Penable-integration-tests test "-Dtest=com.google.cloud.spanner.jdbc.it.**"
