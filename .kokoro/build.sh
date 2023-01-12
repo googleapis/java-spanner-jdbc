ec4af1130cdd24ba4302d0988a1e12a77794861d
Rajat Bhatta <93644539+rajatbhatta@users.noreply.github.com>
2023-01-12 22:59:38 +0530

    update mvn cmd

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -73,11 +73,9 @@ integration-cloud-devel)
     mvn -B ${INTEGRATION_TEST_ARGS} \
       -ntp \
       -Penable-integration-tests \
-      -Djava.net.preferIPv4Stack=true \
       -DtrimStackTrace=false \
       -Dclirr.skip=true \
       -Denforcer.skip=true \
-      -Dmaven.main.skip=true \
       -Dspanner.gce.config.server_url=https://staging-wrenchworks.sandbox.googleapis.com \
       -Dspanner.testenv.instance=projects/span-cloud-testing/instances/spanner-testing-east1 \
       -Dspanner.gce.config.project_id=span-cloud-testing \
