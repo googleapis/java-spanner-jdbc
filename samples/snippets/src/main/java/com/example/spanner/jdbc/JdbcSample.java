4d1bb6958890f42e8aae9a4978b6249a95fbc6c9
Knut Olav Løite <koloite@gmail.com>
2024-03-15 17:16:34 +0100

    docs: create samples for quickstart guide

---
+++ samples/snippets/src/main/java/com/example/spanner/jdbc/JdbcSample.java
@@ -0,0 +1,220 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.spanner.jdbc;
+
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.cloud.spanner.DatabaseId;
+import com.google.cloud.spanner.SpannerExceptionFactory;
+import com.google.cloud.spanner.SpannerOptions;
+import com.google.cloud.spanner.admin.database.v1.DatabaseAdminClient;
+import com.google.cloud.spanner.admin.database.v1.DatabaseAdminSettings;
+import com.google.common.base.Strings;
+import com.google.spanner.admin.database.v1.CreateDatabaseRequest;
+import com.google.spanner.admin.database.v1.DatabaseDialect;
+import com.google.spanner.admin.instance.v1.InstanceName;
+import com.google.spanner.v1.DatabaseName;
+import io.grpc.ManagedChannelBuilder;
+import java.sql.Connection;
+import java.sql.DriverManager;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.Properties;
+import java.util.concurrent.ExecutionException;
+
+public class JdbcSample {
+  // [START spanner_create_database]
+  static void createDatabase(
+      DatabaseAdminClient dbAdminClient,
+      InstanceName instanceName,
+      String databaseId,
+      Properties properties)
+      throws SQLException {
+    // Use the Spanner admin client to create a database.
+    CreateDatabaseRequest createDatabaseRequest =
+        CreateDatabaseRequest.newBuilder()
+            .setCreateStatement("CREATE DATABASE `" + databaseId + "`")
+            .setParent(instanceName.toString())
+            .build();
+    try {
+      dbAdminClient.createDatabaseAsync(createDatabaseRequest).get();
+    } catch (ExecutionException e) {
+      throw SpannerExceptionFactory.asSpannerException(e.getCause());
+    } catch (InterruptedException e) {
+      throw SpannerExceptionFactory.propagateInterrupt(e);
+    }
+
+    // Connect to the database with the JDBC driver and create two test tables.
+    DatabaseName databaseName =
+        DatabaseName.of(instanceName.getProject(), instanceName.getInstance(), databaseId);
+    try (Connection connection =
+        DriverManager.getConnection("jdbc:cloudspanner:/" + databaseName, properties)) {
+      try (Statement statement = connection.createStatement()) {
+        // Create the tables in one batch.
+        statement.addBatch(
+            "CREATE TABLE Singers ("
+                + "  SingerId   INT64 NOT NULL,"
+                + "  FirstName  STRING(1024),"
+                + "  LastName   STRING(1024),"
+                + "  SingerInfo BYTES(MAX),"
+                + "  FullName STRING(2048) AS "
+                + "  (ARRAY_TO_STRING([FirstName, LastName], \" \")) STORED"
+                + ") PRIMARY KEY (SingerId)");
+        statement.addBatch(
+            "CREATE TABLE Albums ("
+                + "  SingerId     INT64 NOT NULL,"
+                + "  AlbumId      INT64 NOT NULL,"
+                + "  AlbumTitle   STRING(MAX)"
+                + ") PRIMARY KEY (SingerId, AlbumId),"
+                + "  INTERLEAVE IN PARENT Singers ON DELETE CASCADE");
+        statement.executeBatch();
+      }
+    }
+    System.out.println("Created database [" + databaseName + "]");
+  }
+  // [END spanner_create_database]
+
+  // [START spanner_postgresql_create_database]
+  static void createPostgreSQLDatabase(
+      DatabaseAdminClient dbAdminClient,
+      InstanceName instanceName,
+      String databaseId,
+      Properties properties)
+      throws SQLException {
+    // Use the Spanner admin client to create a database.
+    CreateDatabaseRequest createDatabaseRequest =
+        CreateDatabaseRequest.newBuilder()
+            // PostgreSQL database names and other identifiers should be quoted using double quotes.
+            .setCreateStatement("create database \"" + databaseId + "\"")
+            .setParent(instanceName.toString())
+            .setDatabaseDialect(DatabaseDialect.POSTGRESQL)
+            .build();
+    try {
+      dbAdminClient.createDatabaseAsync(createDatabaseRequest).get();
+    } catch (ExecutionException e) {
+      throw SpannerExceptionFactory.asSpannerException(e.getCause());
+    } catch (InterruptedException e) {
+      throw SpannerExceptionFactory.propagateInterrupt(e);
+    }
+
+    // Connect to the database with the JDBC driver and create two test tables.
+    DatabaseName databaseName =
+        DatabaseName.of(instanceName.getProject(), instanceName.getInstance(), databaseId);
+    try (Connection connection =
+        DriverManager.getConnection("jdbc:cloudspanner:/" + databaseName, properties)) {
+      try (Statement statement = connection.createStatement()) {
+        // Create the tables in one batch.
+        statement.addBatch(
+            "create table singers ("
+                + "  singer_id   bigint primary key not null,"
+                + "  first_name  varchar(1024),"
+                + "  last_name   varchar(1024),"
+                + "  singer_info bytea,"
+                + "  full_name   varchar(2048) generated always as (\n"
+                + "      case when first_name is null then last_name\n"
+                + "          when last_name  is null then first_name\n"
+                + "          else first_name || ' ' || last_name\n"
+                + "      end) stored"
+                + ")");
+        statement.addBatch(
+            "create table albums ("
+                + "  singer_id     bigint not null,"
+                + "  album_id      bigint not null,"
+                + "  album_title   varchar,"
+                + "  primary key (singer_id, album_id)"
+                + ") interleave in parent singers on delete cascade");
+        statement.executeBatch();
+      }
+    }
+    System.out.println("Created database [" + databaseName + "]");
+  }
+  // [END spanner_postgresql_create_database]
+
+  public static void main(String[] args) throws Exception {
+    if (args.length != 3 && args.length != 4) {
+      printUsageAndExit();
+    }
+    try (DatabaseAdminClient dbAdminClient = createDatabaseAdminClient()) {
+      final String command = args[0];
+      DatabaseId databaseId =
+          DatabaseId.of(SpannerOptions.getDefaultInstance().getProjectId(), args[1], args[2]);
+
+      run(dbAdminClient, command, databaseId);
+    }
+    System.out.println();
+    System.out.println("Finished running sample");
+  }
+
+  static DatabaseAdminClient createDatabaseAdminClient() throws Exception {
+    String emulatorHost = System.getenv("SPANNER_EMULATOR_HOST");
+    if (!Strings.isNullOrEmpty(emulatorHost)) {
+      return DatabaseAdminClient.create(
+          DatabaseAdminSettings.newBuilder()
+              .setTransportChannelProvider(
+                  InstantiatingGrpcChannelProvider.newBuilder()
+                      .setEndpoint(emulatorHost)
+                      .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
+                      .build())
+              .setCredentialsProvider(NoCredentialsProvider.create())
+              .build());
+    }
+    return DatabaseAdminClient.create();
+  }
+
+  static Properties createProperties() {
+    Properties properties = new Properties();
+    String emulatorHost = System.getenv("SPANNER_EMULATOR_HOST");
+    if (!Strings.isNullOrEmpty(emulatorHost)) {
+      properties.put("autoConfigEmulator", "true");
+      properties.put("endpoint", emulatorHost);
+    }
+    return properties;
+  }
+
+  static void run(DatabaseAdminClient dbAdminClient, String command, DatabaseId database)
+      throws Exception {
+    switch (command) {
+      case "createdatabase":
+        createDatabase(
+            dbAdminClient,
+            InstanceName.of(
+                database.getInstanceId().getProject(), database.getInstanceId().getInstance()),
+            database.getDatabase(),
+            createProperties());
+        break;
+      case "createpgdatabase":
+        createPostgreSQLDatabase(
+            dbAdminClient,
+            InstanceName.of(
+                database.getInstanceId().getProject(), database.getInstanceId().getInstance()),
+            database.getDatabase(),
+            createProperties());
+        break;
+      default:
+        printUsageAndExit();
+    }
+  }
+
+  static void printUsageAndExit() {
+    System.err.println("Usage:");
+    System.err.println("    JdbcSample <command> <instance_id> <database_id>");
+    System.err.println();
+    System.err.println("Examples:");
+    System.err.println("    JdbcSample createdatabase my-instance example-db");
+    System.exit(1);
+  }
+}
---
+++ samples/snippets/src/test/java/com/example/spanner/jdbc/JdbcSampleTest.java
@@ -0,0 +1,157 @@
+/*
+ * Copyright 2024 Google LLC
+ *
+ * Licensed under the Apache License, Version 2.0 (the "License");
+ * you may not use this file except in compliance with the License.
+ * You may obtain a copy of the License at
+ *
+ *       http://www.apache.org/licenses/LICENSE-2.0
+ *
+ * Unless required by applicable law or agreed to in writing, software
+ * distributed under the License is distributed on an "AS IS" BASIS,
+ * WITHOUT WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied.
+ * See the License for the specific language governing permissions and
+ * limitations under the License.
+ */
+
+package com.example.spanner.jdbc;
+
+import static com.example.spanner.jdbc.JdbcSample.createDatabase;
+import static com.example.spanner.jdbc.JdbcSample.createPostgreSQLDatabase;
+import static org.junit.Assert.assertEquals;
+
+import com.google.api.gax.core.NoCredentialsProvider;
+import com.google.api.gax.grpc.InstantiatingGrpcChannelProvider;
+import com.google.cloud.spanner.admin.database.v1.DatabaseAdminClient;
+import com.google.cloud.spanner.admin.database.v1.DatabaseAdminSettings;
+import com.google.cloud.spanner.admin.instance.v1.InstanceAdminClient;
+import com.google.cloud.spanner.admin.instance.v1.InstanceAdminSettings;
+import com.google.cloud.spanner.connection.SpannerPool;
+import com.google.spanner.admin.instance.v1.Instance;
+import com.google.spanner.admin.instance.v1.InstanceConfig;
+import com.google.spanner.admin.instance.v1.InstanceName;
+import com.google.spanner.admin.instance.v1.ProjectName;
+import com.google.spanner.v1.DatabaseName;
+import io.grpc.ManagedChannelBuilder;
+import java.io.ByteArrayOutputStream;
+import java.io.PrintStream;
+import java.util.Properties;
+import org.junit.AfterClass;
+import org.junit.BeforeClass;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.junit.runners.JUnit4;
+import org.testcontainers.containers.GenericContainer;
+import org.testcontainers.containers.wait.strategy.Wait;
+import org.testcontainers.utility.DockerImageName;
+
+@RunWith(JUnit4.class)
+public class JdbcSampleTest {
+  private static final String PROJECT_ID = "emulator-project";
+  private static final String INSTANCE_ID = "test-instance";
+  private static final String DATABASE_ID = "test-database";
+  private static final String PG_DATABASE_ID = "pg-test-database";
+
+  private static final ProjectName PROJECT_NAME = ProjectName.of(PROJECT_ID);
+
+  private static final InstanceName INSTANCE_NAME = InstanceName.of(PROJECT_ID, INSTANCE_ID);
+
+  private static GenericContainer<?> emulator;
+
+  private static Properties properties;
+
+  @BeforeClass
+  public static void setup() throws Exception {
+    emulator =
+        new GenericContainer<>(
+                DockerImageName.parse("gcr.io/cloud-spanner-emulator/emulator:latest"))
+            .withExposedPorts(9010)
+            .waitingFor(Wait.forListeningPort());
+    emulator.start();
+    try (InstanceAdminClient client =
+        InstanceAdminClient.create(
+            InstanceAdminSettings.newBuilder()
+                .setTransportChannelProvider(
+                    InstantiatingGrpcChannelProvider.newBuilder()
+                        .setEndpoint(emulator.getHost() + ":" + emulator.getMappedPort(9010))
+                        .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
+                        .build())
+                .setCredentialsProvider(NoCredentialsProvider.create())
+                .build())) {
+      InstanceConfig config =
+          client.listInstanceConfigs(PROJECT_NAME).iterateAll().iterator().next();
+      client
+          .createInstanceAsync(
+              PROJECT_NAME,
+              INSTANCE_ID,
+              Instance.newBuilder()
+                  .setConfig(config.getName())
+                  .setDisplayName("Test Instance")
+                  .setNodeCount(1)
+                  .build())
+          .get();
+    }
+    // Create properties for the JDBC driver to connect to the emulator.
+    properties = new Properties();
+    properties.put("autoConfigEmulator", "true");
+    properties.put("endpoint", emulator.getHost() + ":" + emulator.getMappedPort(9010));
+  }
+
+  @AfterClass
+  public static void cleanup() {
+    SpannerPool.closeSpannerPool();
+    emulator.stop();
+  }
+
+  DatabaseAdminClient createDatabaseAdminClient() throws Exception {
+    return DatabaseAdminClient.create(
+        DatabaseAdminSettings.newBuilder()
+            .setTransportChannelProvider(
+                InstantiatingGrpcChannelProvider.newBuilder()
+                    .setEndpoint(emulator.getHost() + ":" + emulator.getMappedPort(9010))
+                    .setChannelConfigurator(ManagedChannelBuilder::usePlaintext)
+                    .build())
+            .setCredentialsProvider(NoCredentialsProvider.create())
+            .build());
+  }
+
+  @Test
+  public void testCreateDatabase() throws Exception {
+    try (DatabaseAdminClient client = createDatabaseAdminClient()) {
+      String result =
+          runSample(() -> createDatabase(client, INSTANCE_NAME, DATABASE_ID, properties));
+      assertEquals(
+          "Created database [" + DatabaseName.of(PROJECT_ID, INSTANCE_ID, DATABASE_ID) + "]\n",
+          result);
+    }
+  }
+
+  @Test
+  public void testCreatePgDatabase() throws Exception {
+    try (DatabaseAdminClient client = createDatabaseAdminClient()) {
+      String result =
+          runSample(
+              () -> createPostgreSQLDatabase(client, INSTANCE_NAME, PG_DATABASE_ID, properties));
+      assertEquals(
+          "Created database [" + DatabaseName.of(PROJECT_ID, INSTANCE_ID, PG_DATABASE_ID) + "]\n",
+          result);
+    }
+  }
+
+  interface Sample {
+    void run() throws Exception;
+  }
+
+  String runSample(Sample sample) throws Exception {
+    PrintStream stdOut = System.out;
+    ByteArrayOutputStream bout = new ByteArrayOutputStream();
+    PrintStream out = new PrintStream(bout);
+    System.setOut(out);
+    try {
+      sample.run();
+    } finally {
+      System.setOut(stdOut);
+    }
+    return bout.toString();
+  }
+}
