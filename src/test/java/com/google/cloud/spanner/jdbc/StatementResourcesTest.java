a35b7d00be6c1a003999db80b70e6ddcc677ed82
Knut Olav Løite <koloite@gmail.com>
2024-04-15 15:46:45 +0200

    fix: close result set when Statement is closed

--- src/test/java/com/google/cloud/spanner/jdbc/StatementResourcesTest.java
+++ src/test/java/com/google/cloud/spanner/jdbc/StatementResourcesTest.java
@@ -19,6 +19,7 @@ package com.google.cloud.spanner.jdbc;
 import com.google.cloud.spanner.connection.AbstractMockServerTest;
 import java.sql.Connection;
 import java.sql.DriverManager;
+import java.sql.PreparedStatement;
 import java.sql.SQLException;
 import java.sql.Statement;
 import org.junit.Test;
@@ -27,23 +28,61 @@ import org.junit.runners.JUnit4;
 
 @RunWith(JUnit4.class)
 public class StatementResourcesTest extends AbstractMockServerTest {
+  private static final int MIN_SESSIONS = 1;
+  private static final int MAX_SESSIONS = 2;
 
   private String createUrl() {
     return String.format(
         "jdbc:cloudspanner://localhost:%d/projects/%s/instances/%s/databases/%s"
-            + "?usePlainText=true;minSessions=2;maxSessions=2",
-        getPort(), "proj", "inst", "db");
+            + "?usePlainText=true;minSessions=%d;maxSessions=%d",
+        getPort(), "proj", "inst", "db", MIN_SESSIONS, MAX_SESSIONS);
   }
 
   @Test
-  public void testQueryWithExecuteAndNoRead() throws SQLException {
+  public void testMultipleQueriesOnOneStatement() throws SQLException {
     try (Connection connection = DriverManager.getConnection(createUrl())) {
       try (Statement statement = connection.createStatement()) {
-        statement.execute("SELECT 1");
-        statement.execute("SELECT 1");
-        statement.execute("SELECT 1");
+        for (int i = 0; i < MAX_SESSIONS + 1; i++) {
+          // Execute a query without reading or closing the result.
+          statement.execute("SELECT 1");
+        }
       }
     }
   }
 
+  @Test
+  public void testMultipleStatementsWithOneQuery() throws SQLException {
+    try (Connection connection = DriverManager.getConnection(createUrl())) {
+      for (int i = 0; i < MAX_SESSIONS + 1; i++) {
+        try (Statement statement = connection.createStatement()) {
+          // Execute a query without reading or closing the result.
+          statement.execute("SELECT 1");
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testMultipleQueriesOnOnePreparedStatement() throws SQLException {
+    try (Connection connection = DriverManager.getConnection(createUrl())) {
+      try (PreparedStatement statement = connection.prepareStatement("SELECT 1")) {
+        for (int i = 0; i < MAX_SESSIONS + 1; i++) {
+          // Execute a query without reading or closing the result.
+          statement.execute();
+        }
+      }
+    }
+  }
+
+  @Test
+  public void testMultiplePreparedStatementsWithOneQuery() throws SQLException {
+    try (Connection connection = DriverManager.getConnection(createUrl())) {
+      for (int i = 0; i < MAX_SESSIONS + 1; i++) {
+        try (PreparedStatement statement = connection.prepareStatement("SELECT 1")) {
+          // Execute a query without reading or closing the result.
+          statement.execute();
+        }
+      }
+    }
+  }
 }
