def0f89bae7fb3cde66eeb3c0280f9746256ce1c
Knut Olav Løite <koloite@gmail.com>
2024-06-28 08:05:54 +0200

    fix: typo in Docker run command

--- .kokoro/build.sh
+++ .kokoro/build.sh
@@ -48,7 +48,7 @@ if [[ "$JOB_TYPE" == "graalvm" ]]; then
   echo "Starting emulator"
   export SPANNER_EMULATOR_HOST=localhost:9010
   docker pull gcr.io/cloud-spanner-emulator/emulator
-  docker run -d -p --rm --name spanner-emulator 9010:9010 -p 9020:9020 gcr.io/cloud-spanner-emulator/emulator
+  docker run -d --rm --name spanner-emulator -p 9010:9010 -p 9020:9020 gcr.io/cloud-spanner-emulator/emulator
 fi
 
 RETURN_CODE=0
