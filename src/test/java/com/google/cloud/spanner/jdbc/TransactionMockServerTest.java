010a502310db88f8c676960c641a7e9a6581bf11
Knut Olav Løite <koloite@gmail.com>
2025-04-03 09:09:02 +0200

    feat: support isolation level repeatable read (#1973)
    Adds support for setting the transaction isolation level to repeatable read.

--- src/test/java/com/google/cloud/spanner/jdbc/TransactionMockServerTest.java
+++ src/test/java/com/google/cloud/spanner/jdbc/TransactionMockServerTest.java
@@ -17,10 +17,14 @@
 package com.google.cloud.spanner.jdbc;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
 
+import com.google.cloud.spanner.MockSpannerServiceImpl.StatementResult;
 import com.google.cloud.spanner.connection.AbstractMockServerTest;
 import com.google.cloud.spanner.connection.SpannerPool;
 import com.google.spanner.v1.CommitRequest;
+import com.google.spanner.v1.ExecuteSqlRequest;
+import com.google.spanner.v1.TransactionOptions.IsolationLevel;
 import java.sql.Connection;
 import java.sql.DriverManager;
 import java.sql.SQLException;
@@ -45,9 +49,13 @@ public class TransactionMockServerTest extends AbstractMockServerTest {
   }
 
   private String createUrl() {
+    return createUrl("");
+  }
+
+  private String createUrl(String extraOptions) {
     return String.format(
-        "jdbc:cloudspanner://localhost:%d/projects/%s/instances/%s/databases/%s?usePlainText=true;autoCommit=false",
-        getPort(), "proj", "inst", "db");
+        "jdbc:cloudspanner://localhost:%d/projects/%s/instances/%s/databases/%s?usePlainText=true;autoCommit=false%s",
+        getPort(), "proj", "inst", "db", extraOptions);
   }
 
   @Override
@@ -97,5 +105,63 @@ public class TransactionMockServerTest extends AbstractMockServerTest {
     }
 
     assertEquals(0, mockSpanner.countRequestsOfType(CommitRequest.class));
+  }
+
+  @Test
+  public void testUsesDefaultIsolationLevel() throws SQLException {
+    try (Connection connection = createJdbcConnection()) {
+      for (IsolationLevel isolationLevel :
+          new IsolationLevel[] {IsolationLevel.SERIALIZABLE, IsolationLevel.REPEATABLE_READ}) {
+        //noinspection MagicConstant
+        connection.setTransactionIsolation(IsolationLevelConverter.convertToJdbc(isolationLevel));
+        runTestTransaction(connection, isolationLevel);
+      }
+    }
+  }
+
+  @Test
+  public void testUsesManualIsolationLevel() throws SQLException {
+    try (Connection connection = createJdbcConnection()) {
+      connection.setAutoCommit(true);
+      for (IsolationLevel isolationLevel :
+          new IsolationLevel[] {IsolationLevel.SERIALIZABLE, IsolationLevel.REPEATABLE_READ}) {
+        connection
+            .createStatement()
+            .execute(
+                "begin transaction isolation level " + isolationLevel.toString().replace("_", " "));
+        runTestTransaction(connection, isolationLevel);
+      }
+    }
+  }
+
+  @Test
+  public void testUsesDefaultIsolationLevelInConnectionString() throws SQLException {
+    for (IsolationLevel isolationLevel :
+        new IsolationLevel[] {IsolationLevel.SERIALIZABLE, IsolationLevel.REPEATABLE_READ}) {
+      try (Connection connection =
+          DriverManager.getConnection(
+              createUrl(";default_isolation_level=" + isolationLevel.name()))) {
+        runTestTransaction(connection, isolationLevel);
+      }
+    }
+  }
+
+  void runTestTransaction(Connection connection, IsolationLevel expectedIsolationLevel)
+      throws SQLException {
+    String sql = "insert into foo (id) values (1)";
+    mockSpanner.putStatementResult(
+        StatementResult.update(com.google.cloud.spanner.Statement.of(sql), 1L));
+
+    assertEquals(1, connection.createStatement().executeUpdate(sql));
+    connection.commit();
+
+    assertEquals(1, mockSpanner.countRequestsOfType(ExecuteSqlRequest.class));
+    ExecuteSqlRequest request = mockSpanner.getRequestsOfType(ExecuteSqlRequest.class).get(0);
+    assertTrue(request.hasTransaction());
+    assertTrue(request.getTransaction().hasBegin());
+    assertTrue(request.getTransaction().getBegin().hasReadWrite());
+    assertEquals(expectedIsolationLevel, request.getTransaction().getBegin().getIsolationLevel());
+
+    mockSpanner.clearRequests();
   }
 }
